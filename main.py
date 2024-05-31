fae9c029c6177b6ede0105abd1ac8e9632ebacdb
Karol Fornal <162470988+snezz44@users.noreply.github.com>
2024-05-31 16:25:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import make_classification
 from sklearn.base import clone, BaseEstimator, ClassifierMixin
-
+from import_penguins_dataset import import_penguins_dataset
+from import_mushrooms_dataset import import_mushrooms_dataset
 
 class Adaboost(BaseEstimator, ClassifierMixin):
     def __init__(self, T):
@@ -68,77 +69,24 @@ class Tools:
         print("\nDokładne pozycje brakujących wartości (wiersz, kolumna):")
         print(missing_data_positions[missing_data_positions == True])
 
-
 if __name__ == '__main__':
+    # Wczytanie datasetów (można to póżniej zamknąć w jakiejś metodzie, najlepiej na każdy dataset oddzielna)
 
-# Wczytanie datasetów (można to póżniej zamknąć w jakiejś metodzie, najlepiej na każdy dataset oddzielna)
+    # #Chwilowo przydatne do przeformatowanie pliku jak cos###################
+    #     with open("datasets/penguins.csv", "r") as file:
+    #         data = file.read()
+    #
+    #     data = data.replace('"', '')
+    #
+    #     with open("datasets/penguins.csv", "w") as file:
+    #         file.write(data)
+    # #######################################################################
 
-# #Chwilowo przydatne do przeformatowanie pliku jak cos###################
-#     with open("datasets/penguins.csv", "r") as file:
-#         data = file.read()
-#
-#     data = data.replace('"', '')
-#
-#     with open("datasets/penguins.csv", "w") as file:
-#         file.write(data)
-# #######################################################################
+#zaimportowanie pingwinów i grzybów
+    X_penguins, y_penguins = import_penguins_dataset()
+#nrows w grzybach służy do ograniczenia ilość obserwacji, dane są tam przetasowane, a obserwacji jest w sumie ok. 61000
+    X_mushrooms, y_mushrooms = import_mushrooms_dataset(nrows=300)
 
-
-# Wczytanie datasetu z pingwinami (z użyciem pandy łatwiej obrabiać)
-    data_p = pd.read_csv("datasets/penguins.csv", sep=",", skipinitialspace=True, quotechar='"', quoting=1, index_col=False, header=0)
-
-
-################ Rozwiązanie problemu brakujących danych #################
-# Sprawdzenie brakujących danych przed poprawkami
-#     Tools.missingChecker(data_p)
-#     data_p.to_csv("b_f_data_p.csv", index=False)      #data_p przed formatowaniem (before format data_p)
-
-# Usunięcie kolumn "studyName", "Sample Number", "Stage", "Individual ID", "Clutch Completion", "Date Egg", "Comments" bo są useless
-    data_p = data_p.drop(columns=["studyName", "Sample Number", "Stage", "Individual ID", "Clutch Completion", "Date Egg", "Comments"])
-
-# Podział danych na numeryczne i kategoryczne
-    numeric_features = data_p.select_dtypes(include=[np.number]).columns
-    categorical_features = data_p.select_dtypes(include=[object]).columns
-
-# Imputer dla danych numerycznych i kategorycznych
-    categorical_imputer = SimpleImputer(strategy='most_frequent')
-    numeric_imputer = SimpleImputer(strategy='mean')
-
-# Dopasowanie i transformacja danych numerycznych i kategorycznych
-    data_p[numeric_features] = numeric_imputer.fit_transform(data_p[numeric_features])
-    data_p[categorical_features] = categorical_imputer.fit_transform(data_p[categorical_features])
-
-# Przesunięcie kolumny z etykietami ("Species") na ostatnią pozycję
-    data_p['Species'] = data_p.pop('Species')
-
-
-
-
-# Sprawdzenie danych po poprawkach, oraz zapisanie ich w formacie numpy (numpy nie sypie błędów przy kodowaniu etykiet z jakiegoś powodu)
-#     Tools.missingChecker(data_p)
-    data_p.to_csv("penguins_formatted.csv", index=False)
-    data_penguins = np.loadtxt("penguins_formatted.csv", delimiter=",", dtype=object)
-
-# Podział na nazwy kolumn, cechy i etykiety
-    p_column_names = data_penguins[0, :]
-    X_penguins = data_penguins[1:, :-1]
-    y_penguins = data_penguins[1:, -1]
-
-# Konwersja danych kategorycznych na numeryczne
-    encoder = OneHotEncoder(sparse_output=False, drop='first')
-    X_penguins = encoder.fit_transform(X_penguins)
-
-# Zakodowanie etykiet klas
-    y_penguins[y_penguins == 'Adelie Penguin (Pygoscelis adeliae)'] = 0
-    y_penguins[y_penguins == 'Gentoo penguin (Pygoscelis papua)'] = 1
-    y_penguins[y_penguins == 'Chinstrap penguin (Pygoscelis antarctica)'] = 2
-
-    y_penguins = y_penguins.astype(int)
-
-
-# # Dataset z mushroomami
-#     # X_mushrooms, y_mushrooms
-#     # exit(1)
 
 
 # Dane syntetyczne
@@ -148,7 +96,7 @@ if __name__ == '__main__':
 # Słownik datasetów
     d_set = {'Synthetic Data (make_classification)': (X, y),
              'Penguins data set': (X_penguins, y_penguins),
-             # 'mushrooms_data_set': (X_mushrooms, y_mushrooms),
+             'Mushrooms data set': (X_mushrooms, y_mushrooms),
              }
 
 # Słownik klasyfikatorów
